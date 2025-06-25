97fb07348dd9d2009083bd02518403ec4be72d30
Mihir-Arjun <u21458759@tuks.co.za>
2025-06-25 16:04:27 +0200

    Final styling changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -51,13 +51,13 @@ const Todo = () => {
             {/* title */}
             <div className='flex items-center mt-7 gap-2'>
                 <img className='w-8' src={todo_icon} />
-                <h1 className='text-3xl font-semibold'>Todo List</h1>
+                <h1 className='text-3xl font-semibold text-blue-950'>ToDo List</h1>
             </div>
 
             {/* Add tasks */}
             <div className='flex items-center my-7 bg-gray-200 rounded-full'>
                 <input ref = {inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type='text' placeholder='Add your task' />
-                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
+                <button onClick={add} className='border-none rounded-full bg-blue-950 w-16 h-14 text-white text-lg font-medium cursor-pointer'>+</button>
             </div>
 
             {/* todo list */}
