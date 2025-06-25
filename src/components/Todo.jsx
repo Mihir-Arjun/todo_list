0912f735ccf506557d37edb7aa9dd3119f53c4c9
Mihir-Arjun <u21458759@tuks.co.za>
2025-06-25 15:58:29 +0200

    Toggling tasks and making data persist after refresh/restart

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,10 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
 const Todo = () => {
 
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")): [ ]);
 
     const inputRef = useRef();
 
@@ -31,6 +31,21 @@ const Todo = () => {
         })
     }
 
+    const toggle = (id) =>{
+        setTodoList((prevTodos)=>{
+            return prevTodos.map((todo)=>{
+                if(todo.id === id){
+                    return {...todo, isComplete: !todo.isComplete}
+                }
+                return todo;
+            })
+        })
+    }
+
+    useEffect(()=>{
+            localStorage.setItem("todos", JSON.stringify(todoList))
+    },[todoList])
+
     return (
         <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
             {/* title */}
@@ -48,7 +63,7 @@ const Todo = () => {
             {/* todo list */}
             <div>
                 {todoList.map((item, index)=>{
-                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} />
+                    return <TodoItems key={index} text={item.text} id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo} toggle={toggle} />
                 })}
             </div>
 
